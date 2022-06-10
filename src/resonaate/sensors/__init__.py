f846fd010ea25787e9ad29061c866351d25803a6
Jonathan Kadan <jkadan@vt.edu>
2022-06-10 11:11:07 -1000

    updated CHANGELOG and function call docstring

--- src/resonaate/sensors/__init__.py
+++ src/resonaate/sensors/__init__.py
@@ -23,6 +23,7 @@ def sensorFactory(configuration, fov=True):
 
     Args:
         configuration (``dict``): describes the sensor and its capabilities
+        fov (``bool``): Indicator whether or not this sensor should observe all objects in it's FoV
 
     Raises:
         ValueError: raised if invalid option is designate for `"sensor_type"`
