d8dbf4174fc4e3bba62ace56024c9bb5e6869b62
Jonathan Kadan <jkadan@vt.edu>
2022-10-17 19:19:50 -0400

    ugh'

--- src/resonaate/agents/__init__.py
+++ src/resonaate/agents/__init__.py
@@ -1,1 +1,8 @@
 """Package that defines agent-based simulation behavior."""
+
+
+GROUND_FACILITY_LABEL = "GroundFacility"
+"""``str``: Constant string used to describe ground facility sensors."""
+
+SPACECRAFT_LABEL = "Spacecraft"
+"""``str``: Constant string used to describe spacecraft-based sensors."""
