fb99e82f0b0630eb7786a3d48c7af1df5aabb467
Jonathan Kadan <jkadan@vt.edu>
2022-11-22 12:38:46 -0500

    DEP: updated dependancies. closes MR 109 - 120

--- setup.py
+++ setup.py
@@ -32,15 +32,15 @@ setuptools.setup(
             # Linting
             "flake8<4.0",  # Keep flake8 below 4.0 b/c of dep conflict
             "flake8-bugbear==22.10.27",
-            "flake8-builtins==2.0.0",
+            "flake8-builtins==2.0.1",
             "flake8-docstrings==1.6.0",
             "flake8-plugin-utils==1.3.2",
             "flake8-pytest-style==1.6.0",
-            "flake8-rst-docstrings==0.2.7",
-            "pylint==2.15.5",
+            "flake8-rst-docstrings==0.3.0",
+            "pylint==2.15.6",
             # Type Checking
-            "mypy==0.982",
-            "types-sqlalchemy==1.4.53.2",
+            "mypy==0.991",
+            "types-sqlalchemy==1.4.53.8",
             "typing_extensions==4.1.1; python_version < '3.10'",
             # Formatters
             "black==22.10.0",
@@ -62,9 +62,9 @@ setuptools.setup(
         ],
         "doc": [
             "sphinx==5.3.0",
-            "sphinx_rtd_theme==1.0.0",
+            "sphinx_rtd_theme==1.1.1",
             "myst-parser==0.18.1",
-            "sphinx-copybutton==0.5.0",
+            "sphinx-copybutton==0.5.1",
             "sphinxcontrib-bibtex==2.5.0",
             "sphinxcontrib-mermaid==0.7.1",
             "sphinx-gallery==0.11.1",
