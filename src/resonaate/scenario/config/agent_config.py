6496c94551399589e701354df9a9b32061752c12
David Kusterer <kdavid13@vt.edu>
2024-05-13 15:18:48 -0400

    migrate agent_config module to pydantic

--- src/resonaate/scenario/config/agent_config.py
+++ src/resonaate/scenario/config/agent_config.py
@@ -2,12 +2,10 @@
 
 from __future__ import annotations
 
-# Standard Library Imports
-from dataclasses import dataclass
-from typing import ClassVar
+# Third Party Imports
+from pydantic import BaseModel
 
 # Local Imports
-from .base import ConfigObject
 from .platform_config import PlatformConfig
 from .sensor_config import SensorConfig
 from .state_config import StateConfig
@@ -15,13 +13,8 @@ from .state_config import StateConfig
 # ruff: noqa: A003
 
 
-@dataclass
-class AgentConfig(ConfigObject):
+class AgentConfig(BaseModel):
     R"""Configuration base class defining an agent."""
-
-    # Config label class variable - not used by "dataclass"
-    CONFIG_LABEL: ClassVar[str] = "agent"
-    R"""``str``: Key where settings are stored in the configuration dictionary."""
 
     id: int
     R"""``int``: unique ID of the agent."""
@@ -29,44 +22,15 @@ class AgentConfig(ConfigObject):
     name: str
     R"""``str``: name of the agent."""
 
-    state: StateConfig | dict
+    state: StateConfig
     R""":class:`.StateConfig`: defines the location/dynamics of this agent."""
 
-    platform: PlatformConfig | dict
+    platform: PlatformConfig
     R""":class:`.PlatformConfig`: defines the behavior/dynamics of this agent."""
 
-    def __post_init__(self):
-        R"""Runs after the dataclass is initialized."""
-        if isinstance(self.state, dict):
-            self.state = StateConfig.fromDict(self.state)
 
-        if isinstance(self.platform, dict):
-            self.platform = PlatformConfig.fromDict(self.platform, state=self.state)
-
-
-@dataclass
-class TargetAgentConfig(AgentConfig):
-    R"""Configuration object for a target."""
-
-    # Config label class variable - not used by "dataclass"
-    CONFIG_LABEL: ClassVar[str] = "target_agent"
-    R"""``str``: Key where settings are stored in the configuration dictionary."""
-
-
-@dataclass
 class SensingAgentConfig(AgentConfig):
     R"""Configuration object for a :class:`.SensingAgent`."""
 
-    # Config label class variable - not used by "dataclass"
-    CONFIG_LABEL: ClassVar[str] = "sensing_agent"
-    R"""``str``: Key where settings are stored in the configuration dictionary."""
-
-    sensor: SensorConfig | dict
+    sensor: SensorConfig
     R""":class:`.SensorConfig`: defines the sensor object of this sensing agent."""
-
-    def __post_init__(self):
-        R"""Runs after the dataclass is initialized."""
-        super().__post_init__()
-
-        if isinstance(self.sensor, dict):
-            self.sensor = SensorConfig.fromDict(self.sensor)
