8739fa76cee3c04ba690313ef0abbb93326f6f1b
David Kusterer <kdavid13@vt.edu>
2024-06-08 16:56:25 -0400

    migrate geopotential_config module to pydantic

--- src/resonaate/scenario/config/geopotential_config.py
+++ src/resonaate/scenario/config/geopotential_config.py
@@ -3,48 +3,27 @@
 from __future__ import annotations
 
 # Standard Library Imports
-from dataclasses import dataclass
-from typing import ClassVar
+from enum import Enum
 
-# Local Imports
-from .base import ConfigObject, ConfigValueError, inclusiveRange
-
-VALID_GEOPOTENTIAL_MODELS: tuple[str] = (
-    "egm2008.txt",
-    "egm96.txt",
-    "GGM03S.txt",
-    "jgm3.txt",
-)
-DEFAULT_GRAVITY_MODEL: str = "egm96.txt"
-DEFAULT_GRAVITY_DEGREE: int = 4
-DEFAULT_GRAVITY_ORDER: int = 4
-MAX_GRAVITY_DEGREE: int = 80
-MAX_GRAVITY_ORDER: int = 80
+# Third Party Imports
+from pydantic import BaseModel, Field
 
 
-@dataclass
-class GeopotentialConfig(ConfigObject):
+class GeopotentialModel(str, Enum):
+    EGM2008 = "egm2008.txt"
+    EGM96 = "egm96.txt"
+    GGM03S = "GGM03S.txt"
+    JGM3 = "jgm3.txt"
+
+
+class GeopotentialConfig(BaseModel):
     """Configuration section defining several geopotential-based options."""
 
-    CONFIG_LABEL: ClassVar[str] = "geopotential"
-    """``str``: Key where settings are stored in the configuration dictionary."""
-
-    model: str = DEFAULT_GRAVITY_MODEL
+    model: GeopotentialModel = GeopotentialModel.EGM96
     """``str``: Model file used to define the Earth's gravity model."""
 
-    degree: int = DEFAULT_GRAVITY_DEGREE
+    degree: int = Field(default=4, ge=0, le=80)
     """``int``: Degree of the Earth's gravity model."""
 
-    order: int = DEFAULT_GRAVITY_ORDER
+    order: int = Field(default=4, ge=0, le=80)
     """``int``: Order of the Earth's gravity model."""
-
-    def __post_init__(self):
-        """Runs after the object is initialized."""
-        if self.model not in VALID_GEOPOTENTIAL_MODELS:
-            raise ConfigValueError("model", self.model, VALID_GEOPOTENTIAL_MODELS)
-
-        if self.degree not in inclusiveRange(MAX_GRAVITY_DEGREE):
-            raise ConfigValueError("degree", self.degree, inclusiveRange(MAX_GRAVITY_DEGREE))
-
-        if self.order not in inclusiveRange(MAX_GRAVITY_DEGREE):
-            raise ConfigValueError("order", self.order, inclusiveRange(MAX_GRAVITY_ORDER))
