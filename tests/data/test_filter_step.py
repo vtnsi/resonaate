a113f648583a8bfe07a3210a7d723ce84ba371cf
Monty Campbell <mcampbell02@vt.edu>
2024-07-05 14:43:15 -0400

    Updated some unit tests.

--- tests/data/test_filter_step.py
+++ tests/data/test_filter_step.py
@@ -30,8 +30,11 @@ class TestFilterStep:
     nis = 5.37607495178574
     innovation2 = array([-2.334817207660933e-05, -2.0436287793690333e-05])
 
-    eci_state = array([6900, 6900, 6900, -10.0 - 2.03, 8.0])
+    eci_state = array([6900, 6900, 6900, -10.0, -2.03, 8.0])
     q_matrix = array([[1, 2, 3], [1, 2, 3], [4, 5, 6]])
+
+    eci_state_2 = array([6900, -6900, 6900, 10.0, -2.03, 8.0])
+    q_matrix_2 = array([[0, 1, 0], [1, 1, 1], [2, 4, 2]])
 
     def testInit(self):
         """Test the init of FilterStep database table."""
@@ -119,8 +122,8 @@ class TestFilterStep:
             target=target_agent,
             innovation=self.innovation + self.innovation,
             nis=self.nis,
-            truth_eci=self.eci_state,
-            q_matrix=self.q_matrix,
+            truth_eci=self.eci_state_2,
+            q_matrix=self.q_matrix_2,
         )
         # Test equality and inequality
         assert filt1 == filt2
