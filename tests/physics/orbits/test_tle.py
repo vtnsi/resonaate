df6d9ce639a2c264d92b68de520265b59e7f6d99
Monty Campbell <mcampbell02@vt.edu>
2024-08-30 15:04:15 -0400

    Added more unit tests to test_tle.py

--- tests/physics/orbits/test_tle.py
+++ tests/physics/orbits/test_tle.py
@@ -12,9 +12,7 @@ from resonaate.physics.orbits.tle import InitStateLoader
 @pytest.fixture(name="test_tle")
 def testTLE() -> str:
     """Generates the test TLE string."""
-    return """ISS (ZARYA)
-\n1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927
-\n2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537"""  # Generic test TLE
+    return """ISS (ZARYA)\n1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927\n2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537"""  # Generic test TLE
 
 
 def testTLELoad(test_tle: str) -> None:
@@ -22,8 +20,30 @@ def testTLELoad(test_tle: str) -> None:
     InitStateLoader(test_tle)
 
 
-def testInvalidTLELength(test_tle) -> None:
+def testInvalidTLELength(test_tle: str) -> None:
     """Sends a TLE of invalid length to the TLE loader. Should raise an assertion errror."""
     busted_tle = test_tle + "\nI AM BROKEN\nTHIS SHOULD BREAK"
     with pytest.raises(AssertionError):
         InitStateLoader(busted_tle)
+
+
+def testName(test_tle: str) -> None:
+    """Tests the parsin of the satellite's name."""
+    loader = InitStateLoader(test_tle)
+    assert loader.name == "ISS (ZARYA)"
+
+
+def testLoadNoName(test_tle: str) -> None:
+    """Tests to see if the InitStateLoader.name property returns ``None`` if no optional first line is given."""
+    # Get rid of the first optional / name line in the test tle.
+    lines: list[str] = test_tle.split("\n")
+    no_name = f"{lines[1]}\n{lines[2]}"
+    loader = InitStateLoader(no_name)
+    assert loader.name is None
+
+
+def testCatalogNumber(test_tle: str) -> None:
+    """Tests and ensures that the catalog number is being loaded correctly."""
+    loader = InitStateLoader(test_tle)
+    assert type(loader.catalogNumber) is int
+    assert loader.catalogNumber == 25544
