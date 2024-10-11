f08fcdb22bb716f087ec376e7d102e6aab3eedea
David Kusterer <kdavid13@vt.edu>
2024-10-11 13:39:38 -0400

    ignore linter complaining about `Literal` type usage

--- src/resonaate/scenario/config/decision_config.py
+++ src/resonaate/scenario/config/decision_config.py
@@ -15,21 +15,21 @@ from ...common.labels import DecisionLabel
 class MunkresDecisionConfig(BaseModel):
     """Configuration section defining parameters for the munkres decision making algorithm."""
 
-    name: Literal[DecisionLabel.MUNKRES] = DecisionLabel.MUNKRES
+    name: Literal[DecisionLabel.MUNKRES] = DecisionLabel.MUNKRES  # type: ignore
     """``str``: Name of this decision making algorithm."""
 
 
 class MyopicNaiveGreedyDecisionConfig(BaseModel):
     """Configuration section defining parameters for the myopic naive greedy decision making algorithm."""
 
-    name: Literal[DecisionLabel.MYOPIC_NAIVE_GREEDY] = DecisionLabel.MYOPIC_NAIVE_GREEDY
+    name: Literal[DecisionLabel.MYOPIC_NAIVE_GREEDY] = DecisionLabel.MYOPIC_NAIVE_GREEDY  # type: ignore
     """``str``: Name of this decision making algorithm."""
 
 
 class RandomDecisionConfig(BaseModel):
     """Configuration section defining parameters for the random decision making algorithm."""
 
-    name: Literal[DecisionLabel.RANDOM] = DecisionLabel.RANDOM
+    name: Literal[DecisionLabel.RANDOM] = DecisionLabel.RANDOM  # type: ignore
     """``str``: Name of this decision making algorithm."""
 
     seed: Optional[int] = None
@@ -39,7 +39,7 @@ class RandomDecisionConfig(BaseModel):
 class AllVisibleDecision(BaseModel):
     """Configuration section defining parameters for the 'all visible' decision making algorithm."""
 
-    name: Literal[DecisionLabel.ALL_VISIBLE] = DecisionLabel.ALL_VISIBLE
+    name: Literal[DecisionLabel.ALL_VISIBLE] = DecisionLabel.ALL_VISIBLE  # type: ignore
     """``str``: Name of this decision making algorithm."""
 
 
