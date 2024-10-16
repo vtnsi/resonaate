298b36c6a67ddc8007b17777ff7b69a50d6c1d02
David Kusterer <kdavid13@vt.edu>
2024-10-16 12:24:52 -0400

    refactor `FrequencyBand.mean` to utilize kwarg mapping
    that way there's no global calls necessary to set the mapping *and* the kwarg default should only get constructed once, unlike the original implementation of `getFrequencyString()`

--- src/resonaate/physics/sensor_utils.py
+++ src/resonaate/physics/sensor_utils.py
@@ -349,9 +349,6 @@ def calculateMinRadarRange(tx_frequency: float) -> float:
     return (SPEED_OF_LIGHT / tx_frequency / 2) * M2KM
 
 
-_mean_attr: str = "_mean"
-"""str: name of internal attribute for :attr:`.FrequencyBand.mean`."""
-
 class FrequencyBand(str, Enum):
     """Enumeration of supported radar frequency bands."""
 
@@ -368,27 +365,18 @@ class FrequencyBand(str, Enum):
     W = "W"
 
     @property
-    def mean(self) -> float:
+    def mean(self, _mapping={
+        VHF: 165 * 1e6,
+        UHF: 650 * 1e6,
+        L: 1.5 * 1e9,
+        S: 3.0 * 1e9,
+        C: 6.0 * 1e9,
+        X: 10.0 * 1e9,
+        Ku: 15.0 * 1e9,
+        K: 20.0 * 1e9,
+        Ka: 30.0 * 1e9,
+        V: 60.0 * 1e9,
+        W: 15.0 * 1e9,
+    }) -> float:
         """float: Mean frequency of the enumerated band."""
-        return getattr(self, _mean_attr)
-
-    def setMean(self, mean: float):
-        """Set the mean frequency of this enumerated band.
-
-        Args:
-            mean (float): Mean frequency to set this band to.
-        """
-        setattr(self, _mean_attr, mean)
-    
-FrequencyBand.VHF.setMean(165 * 1e6)
-FrequencyBand.VHF.setMean(165 * 1e6)
-FrequencyBand.UHF.setMean(650 * 1e6)
-FrequencyBand.L.setMean(1.5 * 1e9)
-FrequencyBand.S.setMean(3.0 * 1e9)
-FrequencyBand.C.setMean(6.0 * 1e9)
-FrequencyBand.X.setMean(10.0 * 1e9)
-FrequencyBand.Ku.setMean(15.0 * 1e9)
-FrequencyBand.K.setMean(20.0 * 1e9)
-FrequencyBand.Ka.setMean(30.0 * 1e9)
-FrequencyBand.V.setMean(60.0 * 1e9)
-FrequencyBand.W.setMean(15.0 * 1e9)
+        return _mapping[self]
