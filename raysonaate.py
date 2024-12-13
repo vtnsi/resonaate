625fe9f5792361ac627651d359d6c62b8852b92f
David Kusterer <kdavid13@vt.edu>
2024-12-13 14:54:47 -0500

    experimenting with having each ray task process multiple agents' propagation

--- raysonaate.py
+++ raysonaate.py
@@ -2,14 +2,16 @@ from __future__ import annotations
 
 # Standard Library Imports
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, timedelta
+from time import time
 from typing import TYPE_CHECKING
 
 # Third Party Imports
 import ray
-from numpy import ndarray
+from numpy import mean, ndarray
 
 # RESONAATE Imports
+from resonaate.physics.transforms.methods import ecef2lla, eci2ecef
 from resonaate.scenario import buildScenarioFromConfigFile
 
 if TYPE_CHECKING:
@@ -23,9 +25,10 @@ class PropagateSubmission:
 
     agent_id: int
     dynamics: Dynamics
+    init_dt: datetime
     init_time: float
     final_time: float
-    init_state: ndarray
+    init_eci: ndarray
 
 
 @dataclass
@@ -33,21 +36,36 @@ class PropagateResult:
 
     agent_id: int
     final_time: float
-    final_state: ndarray
+    prev_state: ndarray
+    final_eci: ndarray
+    final_ecef: ndarray
+    final_lla: ndarray
 
 
 @ray.remote
-def asyncPropagate(submission: PropagateSubmission):
-    new_state = submission.dynamics.propagate(
-        submission.init_time,
-        submission.final_time,
-        submission.init_state
-    )
-    return PropagateResult(
-        submission.agent_id,
-        submission.final_time,
-        new_state
-    )
+def asyncPropagate(submissions: list[PropagateSubmission]):
+    results = []
+    for submission in submissions:
+        new_eci = submission.dynamics.propagate(
+            submission.init_time,
+            submission.final_time,
+            submission.init_eci
+        )
+        new_dt = submission.init_dt + timedelta(seconds=submission.final_time - submission.init_time)
+        new_ecef = eci2ecef(new_eci, new_dt)
+        new_lla = ecef2lla(new_ecef)
+        results.append(PropagateResult(
+            agent_id=submission.agent_id,
+            final_time=submission.final_time,
+            prev_state=submission.init_eci,
+            final_eci=new_eci,
+            final_ecef=new_ecef,
+            final_lla=new_lla
+        ))
+    return results
+
+SUBMIT_SIZE = 30
+"""Number of RSOs to include in a propagation submission."""
 
 STEP = 300
 
@@ -55,34 +73,46 @@ def rayPropagation():
     ray.init()
     scenario = buildScenarioFromConfigFile("configs/json/main_init.json", start_workers=False)
 
-    print(f"{datetime.now().isoformat()} - Queuing actor propagation...")
+    times = []
+    for it in range(12):
+        start = time()
+        rayPropStep(scenario)
+        times.append(time() - start)
+        print(f"{STEP}s step took {times[it]}")
+    print(f"Mean step duration: {mean(times)}")
+
+
+def rayPropStep(scenario):
     unfinished_tasks = []
+    submit_buffer = []
     for target in scenario.target_agents.values():
-        submission = PropagateSubmission(
-            target.simulation_id,
-            target.dynamics,
-            target.time,
-            target.time + STEP,
-            target.eci_state
+        target: TargetAgent
+        submit_buffer.append(
+            PropagateSubmission(
+                agent_id=target.simulation_id,
+                dynamics=target.dynamics,
+                init_dt=target.datetime_epoch,
+                init_time=target.time,
+                final_time=target.time + STEP,
+                init_eci=target.eci_state
+            )
         )
+        if len(submit_buffer) == SUBMIT_SIZE:
+            unfinished_tasks.append(
+                asyncPropagate.remote(submit_buffer)
+            )
+            submit_buffer = []
+    if submit_buffer:
         unfinished_tasks.append(
-            asyncPropagate.remote(submission)
+            asyncPropagate.remote(submit_buffer)
         )
-    
-    init_task_count = len(unfinished_tasks)
-    print_seg_size = int(init_task_count / 4)
-    completed_task_count = 0
+        del submit_buffer
+
     while unfinished_tasks:
         finished_tasks, unfinished_tasks = ray.wait(unfinished_tasks)
-        result: PropagateResult = ray.get(finished_tasks[0])
-        scenario.target_agents[result.agent_id].time = result.final_time
-        scenario.target_agents[result.agent_id].eci_state = result.final_state
-
-        completed_task_count += 1
-        if completed_task_count % print_seg_size == 0:
-            print(f"Progress: {completed_task_count:03d} / {init_task_count:03d}")
-
-    print(f"{datetime.now().isoformat()} - Done!")
+        results: list[PropagateResult] = ray.get(finished_tasks[0])
+        for result in results:
+            scenario.target_agents[result.agent_id].rayUpdate(result)
 
 
 def sanityCheck():
@@ -98,4 +128,4 @@ def sanityCheck():
 
 
 if __name__ == "__main__":
-    sanityCheck()
+    rayPropagation()
