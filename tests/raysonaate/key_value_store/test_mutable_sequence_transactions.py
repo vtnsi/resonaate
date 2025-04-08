a65b4afb428d724771a68f8f290bee542594a49b
David Kusterer <kdavid13@vt.edu>
2025-04-08 10:25:32 -0400

    more `PopTransaction` tests

--- tests/raysonaate/key_value_store/test_mutable_sequence_transactions.py
+++ tests/raysonaate/key_value_store/test_mutable_sequence_transactions.py
@@ -35,3 +35,30 @@ def testPopTransaction():
     trans.transact(test_dict)
     assert len(test_dict["foo"]) == 1
     assert trans.response_payload == "baz"
+
+
+def testPopTransaction_customIndex():
+    """Test basic use of the :class:`.PopTransaction`."""
+    test_dict = {"foo": ["bar", "baz"]}
+    trans = PopTransaction("foo", 0)
+    trans.transact(test_dict)
+    assert len(test_dict["foo"]) == 1
+    assert trans.response_payload == "bar"
+
+
+def testPopTransaction_noExisting():
+    """Test use of :class:`.PopTransaction` if there's no existing value at specified key."""
+    test_dict = {}
+    trans = PopTransaction("foo")
+    trans.transact(test_dict)
+    assert trans.error is None
+    assert trans.response_payload is None
+
+
+def testPopTransaction_existingNonSequence():
+    """Test use of :class:`.PopTransaction` if the existing value isn't a mutable sequence."""
+    test_dict = {"foo": 123}
+    trans = PopTransaction("foo")
+    trans.transact(test_dict)
+    assert trans.response_payload is None
+    assert trans.error is not None
